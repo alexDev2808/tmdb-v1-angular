ba3b6ed435802bd173fa34822a9e7d6bc51a9dac
AlexDev2808 <colores.2808.8000@gmail.com>
2023-05-25 10:01:08 -0600

    Ciclo de vida de los componentes

--- src/app/components/img/img.component.ts
+++ src/app/components/img/img.component.ts
@@ -1,11 +1,11 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input, Output, EventEmitter, OnInit, AfterViewInit OnChanges, SimpleChanges, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-img',
   templateUrl: './img.component.html',
   styleUrls: ['./img.component.scss']
 })
-export class ImgComponent {
+export class ImgComponent implements OnInit, OnDestroy, OnChanges, AfterViewInit {
 
   // con input podemos recibir datos del padre
   @Input() img: string = 'url de la img';
@@ -15,7 +15,33 @@ export class ImgComponent {
 
   defaultImg = 'https://www.idsplus.net/wp-content/uploads/default-placeholder.png'
 
+  constructor() {
+    // antes del render, no correr cosas asincronas, corre una vez
+    console.log('constructor');
 
+  }
+  ngOnChanges(changes: SimpleChanges): void {
+    // antes y durante el render, acutualizando cambios en inputs
+    console.log("ngOnChanges");
+
+  }
+  ngOnInit(): void {
+    // antes del render, cosas asincronas, corre una vez
+    console.log("ngOnInit");
+
+  }
+
+  ngAfterViewInit(): void {
+    // despues del render, manejar a los hijos
+    console.log("AfterViewInit");
+
+  }
+
+  ngOnDestroy(): void {
+    // eliminar componente
+    console.log("ngOnDestroy");
+
+  }
   imgError() {
     this.img = this.defaultImg;
   }
